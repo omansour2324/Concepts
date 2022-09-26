014cf4702ac8d977534fd11e9c037b8ffc4fd708
omansour2324 <omansour2324@gmail.com>
2022-09-25 21:30:44 -0400

    SQueue & SStack - Initial - Setup

---
+++ Concepts.Library/Datastructures/Basic/SQueue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concepts.Library.Datastructures.Basic
+{
+    //TODO: Queue using array
+    public class SQueueArray
+    {
+    }
+
+    //TODO: Queue using linkedlist
+    public class SQueueList
+    {
+    }
+}
---
+++ Concepts.Library/Datastructures/Basic/SStack.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concepts.Library.Datastructures.Basic
+{
+    //TODO: Stack with Array
+    public class SStackArray
+    {
+    }
+
+    //TODO: Stack with list
+    public  class SStackList
+    {
+    }
+}
