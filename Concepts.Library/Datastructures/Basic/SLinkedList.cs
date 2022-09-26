62b04304f2d2e2a1324dd05e17aa36897bc3992c
omansour2324 <omansour2324@gmail.com>
2022-09-25 21:23:55 -0400

    SLinkedList - Imp & Tests - V1

--- Concepts.Library/Datastructures/Basic/SLinkedList.cs
+++ Concepts.Library/Datastructures/Basic/SLinkedList.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,10 +19,114 @@ namespace Concepts.Library.Datastructures.Basic
 
     public  class SLinkedList<T>
     {
-        public void Add() {}
-        public T Find() { return default; }
-        public void Delete() { }
-        public void Update() { }
+        public SNode<T> Head;
+        public int Length;
+
+        public SLinkedList()
+        {
+            Length = 0;
+            Head = null;
+        }
+
+        public void AddFirst(T value) 
+        {
+            var newElement = new SNode<T>(value);
+
+            if (Head == null)
+            {
+                Head = newElement;
+            }
+            else
+            {
+                newElement.Next = Head;
+                Head = newElement;
+            }
+
+            Length++;
+        }
+        public void AddLast(T value) 
+        {
+            var newElement = new SNode<T>(value);
+
+            if (Head == null)
+            {
+                Head = newElement;
+            }
+            else
+            {
+                var current = Head;
+                while(current.Next != null)
+                {
+                    current = current.Next;
+                }
+
+                current.Next = newElement;
+            }
+
+            Length++;
+        }
+        public int Find(T value) {
+
+            int index = 0;
+            var current = Head;
+
+            while(current.Next != null && !current.Value.Equals(value))
+            {
+                current = current.Next;
+                index++;
+            }
+
+            if (!current.Value.Equals(value))
+                index = -1;
+
+            return index;
+        }
+        
+        public T Update(T oldValue, T newValue) 
+        {
+            var current = Head;
+            while(current.Next != null && !current.Value.Equals(oldValue))
+            {
+                current = current.Next;
+            }
+
+            if (current.Value == null || !current.Value.Equals(oldValue))
+                throw new Exception("Value Does Not Exist");
+
+            current.Value = newValue;
+            return current.Value;
+        }
+
+
+        public void Delete(T value)
+        {
+            if (Head == null)
+            {
+                return;
+            }
+
+            if(Head!= null)
+            {
+                var current = Head;
+                SNode<T> previous = null;
+
+                while(current.Next != null && !current.Value.Equals(value))
+                {
+                    previous = current;
+                    current = current.Next;
+                }
+
+                if (current.Value == null || !current.Value.Equals(value))
+                {
+                    return;
+                }
+                else
+                {
+                    previous.Next = current.Next;
+                    Length--;
+                }
+            }
+        }
 
     }
 }
--- Concepts.UnitTests/Datastructures_UT/SLinkedList_UT.cs
+++ Concepts.UnitTests/Datastructures_UT/SLinkedList_UT.cs
@@ -9,13 +9,54 @@ namespace Concepts.UnitTests.Datastructures_UT
         [SetUp]
         public void Setup()
         {
-         
+            _testLinkedList = new SLinkedList<int>();
         }
 
         [Test]
-        public void PlaceHolderTest()
+        public void AddFirst_WhenListEmpty_ValueAdded()
         {
+            _testLinkedList.AddFirst(1);
 
+            Assert.That(_testLinkedList.Length, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void AddLast_WhenListEmpty_ValueAdded()
+        {
+            _testLinkedList.AddLast(1);
+
+            Assert.That(_testLinkedList.Length, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Find_WhenHasValue_FoundValue()
+        {
+            _testLinkedList.AddLast(1);
+
+            Assert.That(_testLinkedList.Find(1), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Update_WhenHasValue_UpdateValue()
+        {
+            _testLinkedList.AddLast(1);
+            _testLinkedList.AddLast(10);
+
+            var result = _testLinkedList.Update(10, 12);
+
+            Assert.That(result, Is.EqualTo(12));
+        }
+
+        [Test]
+        public void Delete_WhenHasValues_DeleteValue()
+        {
+            _testLinkedList.AddLast(1);
+            _testLinkedList.AddLast(10);
+            _testLinkedList.AddLast(12);
+
+            _testLinkedList.Delete(10);
+
+            Assert.That(-1, Is.EqualTo(_testLinkedList.Find(10)));
         }
     }
 }
