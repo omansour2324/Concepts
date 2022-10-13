b301b030cc73113033a0e263054045662df40580
Osama M <Osama.Mansour@cpsi.com>
2022-10-13 18:11:37 -0500

    Sliding Window

---
+++ Concept.Console/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Concepts.Library.Algorithms.Patterns;
+
+SlidingWindow slidingWindow = new SlidingWindow();
+slidingWindow.Run();
---
+++ Concepts.Library/Algorithms/Patterns/SlidingWindow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concepts.Library.Algorithms.Patterns
+{
+    public class SlidingWindow
+    {
+        public void Run()
+        {
+            int[] arr = new int[] { 4, 9, 10, 19, 12, 3 };
+            int n = arr.Length;
+            int k = 2;
+
+
+            for(int i = 0; i <= n-k; i++)
+            {
+                for(int j = 0; j < k; j++)
+                {
+                    Console.Write($"{arr[i + j]},");
+                }
+
+                Console.WriteLine();
+
+            }
+
+        }
+    }
+}
